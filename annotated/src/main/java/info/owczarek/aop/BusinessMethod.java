d7d6353caa071c310017f95c253b562e967354a1
Artur Owczarek <owczarek.artur@gmail.com>
2015-02-21 18:05:32 +0100

    Omówienie desygnatorów

---
+++ annotated/src/main/java/info/owczarek/aop/BusinessMethod.java
@@ -0,0 +1,8 @@
+package info.owczarek.aop;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.RUNTIME)
+public @interface BusinessMethod {
+}
---
+++ execution/src/main/java/info/owczarek/aop/Main.java
@@ -0,0 +1,42 @@
+package info.owczarek.aop;
+
+import info.owczarek.aop.package1.AnotherBean;
+import info.owczarek.aop.package1.subpackage.ThirdBean;
+import info.owczarek.aop.package2.SomeBean;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Main {
+
+	public static void main(String[] args) {
+		ApplicationContext context = new ClassPathXmlApplicationContext(
+				"context.xml");
+
+		SomeBean someBean = context.getBean("someBean", SomeBean.class);
+		AnotherBean anotherBean = context.getBean("anotherBean", AnotherBean.class);
+		ThirdBean thirdBean = context.getBean("thirdBean", ThirdBean.class);
+		
+		someBean.setSomething(null);
+		someBean.setAnotherThing(null);
+		someBean.getSomething();
+		someBean.anotherMethod();
+		someBean.setInteger(1);
+		someBean.setTwoParameters(null, null);
+		
+		anotherBean.setSomething(null);
+		anotherBean.setAnotherThing(null);
+		anotherBean.getSomething();
+		anotherBean.anotherMethod();
+		anotherBean.setInteger(1);
+		anotherBean.setTwoParameters(null, null);
+		
+		thirdBean.setSomething(null);
+		thirdBean.setAnotherThing(null);
+		thirdBean.getSomething();
+		thirdBean.anotherMethod();
+		thirdBean.setInteger(1);
+		thirdBean.setTwoParameters(null, null);
+	}
+
+}
